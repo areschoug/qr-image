07a77ee8e7b1b443afc07327f43a93fd5063639b
Andreas Areschoug <andreas.areschoug@gmail.com>
2020-12-23 11:46:14 +0100

    Only support SVG-Object

--- tests/test.js
+++ tests/test.js
@@ -10,8 +10,8 @@ var text = 'I \u2764\uFE0F QR code!';
 var text = 'https://yadi.sk/d/FuzPeEg-QyaZN?qr';
 var ec_level = 'Q';
 
-qr.image(text, { type: 'svg', ec_level: ec_level}).pipe(file('qr.svg'));
-qr.image(text, { type: 'eps', ec_level: ec_level}).pipe(file('qr.eps'));
-qr.image(text, { type: 'pdf', ec_level: ec_level}).pipe(file('qr.pdf'));
+// qr.image(text, { type: 'svg', ec_level: ec_level}).pipe(file('qr.svg'));
+// qr.image(text, { type: 'eps', ec_level: ec_level}).pipe(file('qr.eps'));
+// qr.image(text, { type: 'pdf', ec_level: ec_level}).pipe(file('qr.pdf'));
 
 // fs.writeFileSync('qr_sync.svg', qr.imageSync(text, {type: 'svg'}));
